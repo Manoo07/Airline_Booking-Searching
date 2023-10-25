4d9cfa861f4011601624287268e15bad7f20ed32
Manoo07 <manoharboinapalli1196@gmail.com>
2023-10-25 08:18:15 +0530

    Added a basic bodyParser middleware

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const { PORT } = require('./config/serverConfig');
+const bodyparser = require('body-parser');
 
 require('dotenv').config();
 
 const setUpAndStartServer = async () =>{
 
     const app = express();
+    app.use(bodyparser.json());
+    app.use(bodyparser.urlencoded({extended: true}));
     // const PORT = 3000;
     app.listen(PORT,()=>{
         console.log('Server started at : ',PORT);
